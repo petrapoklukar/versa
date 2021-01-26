cf82611a8d972813c6de9b22320c79658a562445
Petra Poklukar <petra.poklukar@gmail.com>
2021-01-26 22:13:46 +0100

    Prepared GPU training

--- src/train_on_cluster.sh
+++ src/train_on_cluster.sh
@@ -30,7 +30,7 @@ nvidia-smi
 
 python3 run_classifier.py --mode "train_test" --dataset "celebA" \
                           --d_theta 64 \
-                          --way -2 -s-hot 5 --test_shot 5 --test_way 2 \
+                          --way -2 --shot 5 --test_shot 5 --test_way 2 \
                           --tasks_per_batch 6 --samples 10 \
                           --learning_rate 1e-4 --iterations 10000 \
                           --dropout 0.9 \
