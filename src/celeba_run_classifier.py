66e0eef7ea308c0412b77446c35a3094debc0274
Petra Poklukar <petra.poklukar@gmail.com>
2021-01-29 11:44:53 +0100

    Adjusted CelebA testing

--- src/celeba_run_classifier.py
+++ src/celeba_run_classifier.py
@@ -352,13 +352,10 @@ def main(unused_argv):
                             dropout_keep_prob: 1.0}
 
                 ft, wm, bm, wlv, blv = sess.run(output_tensors, feedDict)
-                print(wm.shape, wlv.shape) # (3, 64, 2)
-                print(bm.shape, blv.shape) # (3, 2)
-                print(ft.shape) # (3, 10, 64)
-                logits_mean_test = tf.matmul(ft, wm) + bm # (3, 10, 64) * (3, 64, 2)
-                logits_log_var_test = tf.log(tf.matmul(ft ** 2, tf.exp(wlv)) + tf.exp(blv))
-                logits_sample_test = sample_normal(logits_mean_test, logits_log_var_test, 11)
-                print('TEST: ', logits_mean_test.shape, logits_log_var_test.shape, logits_sample_test.shape)
+                # logits_mean_test = tf.matmul(ft, wm) + bm # (3, 10, 64) * (3, 64, 2)
+                # logits_log_var_test = tf.log(tf.matmul(ft ** 2, tf.exp(wlv)) + tf.exp(blv))
+                # logits_sample_test = sample_normal(logits_mean_test, logits_log_var_test, 11)
+                # print('TEST: ', logits_mean_test.shape, logits_log_var_test.shape, logits_sample_test.shape)
         
                 n_samples = 40
                 n_marginal = 100
@@ -375,7 +372,11 @@ def main(unused_argv):
                     # TODO: are w and b correct?
                     w = wm + np.random.normal(loc=0.0, scale=1.0, size=1) * np.sqrt(np.exp(wlv))  # sample from normal(weight_mean, exp(weight_log_variance))
                     b = bm + np.random.normal(loc=0.0, scale=1.0, size=1) * np.sqrt(np.exp(blv))  # sample from normal (bias_mean, exp(bias_log_variance))
+                    print(b.reshape(3, 1, 2), np.matmul(ft, w))
                     outputs = np.matmul(ft, w) + b.reshape(3, 1, 2)
+                    print(wm.shape, wlv.shape) # (3, 64, 2)
+                    print(bm.shape, blv.shape) # (3, 2)
+                    print(ft.shape) # (3, 10, 64)
                     # cluster assignments can also be done with marginal
                     task_probs = celeba_test_utils.np_softmax(outputs[0])
                     _task_log_probs = np.log(task_probs[np.where(test_outputs == 1)]).reshape([3, -1])
